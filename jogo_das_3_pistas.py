a93608546dbc6e0bba52dc939ff7555e496daf58
Prata <prata.infinity@gmail.com>
2024-10-28 11:53:17 -0300

    jogo básico funcionando corretamente

--- jogo_das_3_pistas.py
+++ jogo_das_3_pistas.py
@@ -3,6 +3,11 @@ dicionario = {
         "dica_1": "é um sobrenome",
         "dica_2": "corta madeira",
         "dica_3": "de assis"
+    },
+    "são paulo": {
+        "dica_1": "nome de santo",
+        "dica_2": "nome de cidade",
+        "dica_3": "nome de time"
     }
 }
 
@@ -11,14 +16,22 @@ for palavra_secreta, dicas in dicionario.items():
     chute_usuario = input('Digite a palavra secreta: ')
     if chute_usuario == palavra_secreta:
         print('parabéns. Acertou')
-
+    # ERRO DICA 1
     else:
         print('Você errou.')
         print(f"DICA 2: {dicas['dica_2']}")
         chute_usuario = input('Digite a palavra secreta: ')
         if chute_usuario == palavra_secreta:
             print('parabéns. Acertou')
+        # ERRO DICA 2    
         else:
             print('Você errou.')
+            print(f"DICA 3: {dicas['dica_3']}")
+            chute_usuario = input('Digite a palavra secreta: ')
+            if chute_usuario == palavra_secreta:
+                print('parabéns. Acertou')
+            # ERRO DICA 3
+            else:
+                print('Você errou.')
 
 
